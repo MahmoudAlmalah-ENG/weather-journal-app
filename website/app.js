c4af89f54a56367b258b27fbaa7d75b6f6380013
Mahmoud Almalah <75377167+mahmoudalmalah37@users.noreply.github.com>
2023-02-07 19:30:33 +0200

    new

--- website/app.js
+++ website/app.js
@@ -2,13 +2,11 @@
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
 // Personal API Key for OpenWeatherMap API
-function apiKey() {
-    return 'b0b06dbf01aaab2489ef2903c1336134';
-}
+const apiKey = 'b0b06dbf01aaab2489ef2903c1336134&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+ 1 +' / '+ d.getDate()+' / '+ d.getFullYear();
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performAction);
@@ -17,7 +15,7 @@ document.getElementById('generate').addEventListener('click', performAction);
 function performAction(e) {
     const zip = document.getElementById('zip').value;
     const feelings = document.getElementById('feelings').value;
-    getWeather(baseURL, zip, apiKey())
+    getWeather(baseURL, zip, apiKey)
     .then(function(data) {
         postData('/add', {date: newDate, temp: data.main.temp, content: feelings});
     })
